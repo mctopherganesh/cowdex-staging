356978c8c7f2ab339823b65fd9479f4e9ee7b0b6
mctopherganesh <mctopherganesh@gmail.com>
2018-03-30 16:28:14 -0400

    cut the head off index.html and put it into another folder

---
+++ includes/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<html>
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-114046963-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-114046963-1');
+	</script>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="cover.css" rel="stylesheet">
+
+
+	<title><?php echo "Cowdex"; ?></title>
+	<header>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="/index.html">CowDex</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    			<ul class="navbar-nav mr-auto">
+      				<li class="nav-item active">
+        				<a class="nav-link" href="/index.html">Home <span class="sr-only">(current)</span></a>
+      				</li>
+      				<li class="nav-item">
+        				<a class="nav-link" href="/archive.html">Archive</a>
+      				</li>
+      			</ul>
+      		</div>
+		</nav>
+		
+			
+		<h1 class="">My Cow Index Project!</h1>
+			
+		
+	</header>
+</head>
